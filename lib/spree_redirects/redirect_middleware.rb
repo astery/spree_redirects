174fa3a4952ab446bf2d61b4cf002abc7e714cc0
Luis Doubrava <luis@cg.nl>
2013-11-26 12:08:28 +0100

    only raise exception when not an ActiveRecord::NotFound exception

--- lib/spree_redirects/redirect_middleware.rb
+++ lib/spree_redirects/redirect_middleware.rb
@@ -1,10 +1,10 @@
 module SpreeRedirects
   class RedirectMiddleware
-    
+
     def initialize(app)
       @app = app
     end
-   
+
     def call(env)
       # when consider_all_requests_local is false, an exception is raised for 404
       # consider_all_requests_local should be false in a production environment
@@ -22,17 +22,16 @@ module SpreeRedirects
           # Issue a "Moved permanently" response with the redirect location
           return [ 301, { "Location" => url }, [ "Redirecting..." ] ]
         end
+        raise routing_error unless routing_error.blank? || routing_error.is_a?(ActiveRecord::RecordNotFound)
       end
-
-      raise routing_error if routing_error.present?
 
       [ status, headers, body ]
     end
-    
+
     def find_redirect(url)
       redirect = Spree::Redirect.find_by_old_url(url)
       redirect.new_url unless redirect.nil?
     end
-    
+
   end
-end 
+end
